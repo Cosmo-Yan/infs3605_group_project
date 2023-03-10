107e2c39efd10d93c9bf89fd8a7a1b82b5e3896d
CptFubuki <99645983+CptFubuki@users.noreply.github.com>
2023-03-10 16:44:51 +1100

    To do:
    - Create a new version of the form using edit texts (WIP)
    - Add borders to the edit text views
    - Add drop down menus (1/2 complete)
    - Automatically create a new line when the user is types a long response (done)
    - Add the Save and Next buttons and lock them in place (done)
    - Make the form prettier
    - Add a drawer

--- app/src/main/java/com/example/infs3605_group_project/FormV2Controller.java
+++ app/src/main/java/com/example/infs3605_group_project/FormV2Controller.java
@@ -14,11 +14,19 @@ public class FormV2Controller extends AppCompatActivity {
         setContentView(R.layout.form_v2);
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        // Create an ArrayAdapter using the string array and a default spinner layout
+        // Creates an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.events_array, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+        spinner.setPrompt("Select an event*");
+
+        spinner.setAdapter(
+                new FormV2Adapter(
+                        adapter,
+                        R.layout.contact_spinner_row_nothing_selected,
+                        // R.layout.contact_spinner_nothing_selected_dropdown, // Optional
+                        this));
     }
 }
